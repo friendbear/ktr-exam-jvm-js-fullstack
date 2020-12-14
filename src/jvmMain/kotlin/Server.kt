a60c1abdda56c5cfb202ebba7fcf4d10f96e87ec
friendbear <friendbear@users.noreply.github.com>
2020-12-14 14:28:32 +0900

    ContentNegotiation, CORS, Compression Settings

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,6 +1,9 @@
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import java.security.KeyStore
@@ -12,7 +15,18 @@ fun main() {
             get("/hello") {
                 call.respondText("Hello, API!")
             }
-
+        }
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
         }
     }.start(wait = true)
 
